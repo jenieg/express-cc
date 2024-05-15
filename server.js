0fff938657d746a15df37ac9158d8ec077511d8e
Jennifer <jmarthagodfrey@gmail.com>
2024-05-15 12:30:28 -0600

    express-cc

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url'
 import posts from './routes/posts.js'
 import logger from './middleware/logger.js'
 import errorHandler from './middleware/error.js';
 import notFound from './middleware/notFound.js';
 
 const port = process.env.PORT || 8000;
+
+//Get directory name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -16,7 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(logger);
 
 //Setup Static Folder
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Routes
 app.use('/api/posts', posts);
